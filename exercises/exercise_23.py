4f87982caccdec1c5e29b3247270513c227d18ca
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-13 12:51:40 -0400

    Update exercise_23.py

--- exercises/exercise_23.py
+++ exercises/exercise_23.py
@@ -1,1 +1,15 @@
-# Your solution to Exercise 23
+sum = 0
+
+inputed = 1
+num_of_inputs = 0
+
+while inputed != 0:
+    inputed = int(input())
+    sum += inputed
+    if inputed != 0:
+        num_of_inputs += 1
+
+if num_of_inputs != 0:
+    print(sum/num_of_inputs)
+else: 
+    print(0)

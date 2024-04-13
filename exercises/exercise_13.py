d321387d5b9d0a261a74d84e33b2889786ce9754
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-13 12:31:01 -0400

    Update exercise_13.py

--- exercises/exercise_13.py
+++ exercises/exercise_13.py
@@ -1,1 +1,9 @@
-# Your solution to Exercise 13
+password = int(input("Please enter your password: "))
+
+while True:
+    password_guess = int(input("Please enter your password: "))
+    if password_guess == password:
+        print("Done")
+        break
+    else:
+        print("Error")

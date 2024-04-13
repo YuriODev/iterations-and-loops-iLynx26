0f32e473fd8a45946194bd2d3cc7f421cce70fd2
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-13 12:31:55 -0400

    Update exercise_8.py

--- exercises/exercise_8.py
+++ exercises/exercise_8.py
@@ -1,1 +1,11 @@
-# Your solution to Exercise 8
+n = int(input())
+
+line = ""
+
+for i in range(1, n + 1):
+    if i % 2 == 0:
+        line += str(i) + " " 
+    else: 
+        continue
+
+print(line)

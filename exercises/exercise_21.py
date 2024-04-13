6b7e8911a08e82dc94dbe394e42cf3373f2a8532
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-13 12:31:29 -0400

    Update exercise_21.py

--- exercises/exercise_21.py
+++ exercises/exercise_21.py
@@ -1,1 +1,13 @@
-# Your solution to Exercise 21
+n = int(input())
+
+sum = 0
+for i in range(1, n + 1):
+
+    factorial = 1
+    for j in range(1, i + 1):
+        factorial *= j
+    
+    sum += factorial
+
+print(sum)
+

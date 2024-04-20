849e1ee1d149f5996ece909b87324ee62156fc24
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 12:55:43 -0400

    Update exercise_43.py

--- exercises/exercise_43.py
+++ exercises/exercise_43.py
@@ -1,9 +1,8 @@
 biggest_number = 0
 second_biggest_number = 0
 current_number = 0
-infinite_number = 9999
 
-for i in range(infinite_number):
+while True:
     current_number = int(input())
     if current_number >= biggest_number:
         second_biggest_number = biggest_number
@@ -12,3 +11,4 @@ for i in range(infinite_number):
             second_biggest_number = current_number
     if current_number == 0:
         print(second_biggest_number)
+        break

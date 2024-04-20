0b9776303fe79ddd10d85e740c46781d6cee2311
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 11:11:07 -0400

    Update exercise_47.py

--- exercises/exercise_47.py
+++ exercises/exercise_47.py
@@ -1,1 +1,15 @@
-# Your solution to Exercise 47
+n = int(input())
+
+output = ""
+
+for i in range(n):
+    if len(i) == 1:
+        output += i
+    if len(i) == 2:
+        if n % 10 == n//10:
+            output += i
+    if len(i) == 3:
+        if n % 10 == n//100:
+            output += i
+
+print(output)

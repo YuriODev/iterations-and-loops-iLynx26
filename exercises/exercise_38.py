ae71b2ee1b47e2e022c3f80357a253f6b9fe8c06
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 11:10:35 -0400

    Update exercise_38.py

--- exercises/exercise_38.py
+++ exercises/exercise_38.py
@@ -1,1 +1,20 @@
-# Your solution to Exercise 38
+number = abs(int(input()))
+
+biggest_digit = 0
+smallest_digit = 0
+
+for i in range(10):
+    digit = number%(10**(i+1))//(10**i)
+
+    if digit > biggest_digit:
+        biggest_digit = digit
+    if digit < smallest_digit:
+        smallest_digit = digit
+
+    if number//(10**(i)) < 10:
+        break
+
+if (biggest_digit - smallest_digit) % 2 == 0:
+    print(False)
+else:
+    print(True)

ef9a6ba2e00f3b78a3e0e2bc7b016806ad633f2a
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 11:10:42 -0400

    Update exercise_40.py

--- exercises/exercise_40.py
+++ exercises/exercise_40.py
@@ -1,1 +1,18 @@
-# Your solution to Exercise 40
+number = int(input())
+
+current_number = 1
+previous_number = 1
+
+inf = 9999
+
+output = "1 1 "
+
+for i in range(inf):
+    current_number = current_number + previous_number
+    previous_number = current_number - previous_number
+    if current_number < number:
+        output += f"{current_number} "
+    else:
+        break
+
+print(output)

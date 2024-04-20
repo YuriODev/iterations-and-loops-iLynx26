2acd77efd35e67e230ceb6bff7ae2d509018247f
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 11:27:41 -0400

    Update exercise_33.py

--- exercises/exercise_33.py
+++ exercises/exercise_33.py
@@ -1,1 +1,9 @@
-# Your solution to Exercise 33
+n = int(input('Please, give me your number: '))
+a = 0
+b = n - 1
+
+
+for i in range(n):
+    print('-1\t' * a + '0\t' + '1\t' * b)
+    a += 1
+    b -= 1

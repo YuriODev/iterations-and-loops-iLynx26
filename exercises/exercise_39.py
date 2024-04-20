47a92aa0eb840c62dcaf2401be266897f9cf2a83
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 11:10:38 -0400

    Update exercise_39.py

--- exercises/exercise_39.py
+++ exercises/exercise_39.py
@@ -1,1 +1,10 @@
-# Your solution to Exercise 39
+number = abs(int(input()))
+
+sum_of_digits = 0
+
+for i in range(10):
+    sum_of_digits += number%(10**(i+1))//(10**i)
+    if number//(10**(i)) < 10:
+        break
+
+print(sum_of_digits)

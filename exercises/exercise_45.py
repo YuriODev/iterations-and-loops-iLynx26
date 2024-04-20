4afe9c835b8f02aaa558a57b0b08c5359c90b262
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 11:11:11 -0400

    Update exercise_45.py

--- exercises/exercise_45.py
+++ exercises/exercise_45.py
@@ -1,1 +1,17 @@
-# Your solution to Exercise 45
+sign_changed = 0
+
+
+current_number = 0
+previous_number = 0
+
+while True:
+    current_number = int(input())
+    if current_number == 0:
+        print(sign_changed)
+        break
+    elif previous_number == 0:
+        continue
+    else:
+        if previous_number/abs(previous_number) + current_number/abs(current_number) == 0:
+            sign_changed += 1
+

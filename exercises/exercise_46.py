9938c59f6c86f2059b1a96e3c8fc4f054b969659
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 11:11:08 -0400

    Update exercise_46.py

--- exercises/exercise_46.py
+++ exercises/exercise_46.py
@@ -1,1 +1,9 @@
-# Your solution to Exercise 46
+number = int(input())
+digit = int(input())
+
+if digit in number.split(""):
+    for i in range(10):
+        if i%(10**(i+1))//(10**i) == digit:
+            print(i+1)
+else:
+    print(0)

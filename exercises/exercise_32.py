b93e5843545e8c7057bb972d5f81feb0088d6741
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 11:10:22 -0400

    Update exercise_32.py

--- exercises/exercise_32.py
+++ exercises/exercise_32.py
@@ -1,1 +1,17 @@
-# Your solution to Exercise 32
+cars = int(input())
+
+cars_following_rules = 0
+fastest_car = 0
+slowest_car = 999
+
+for i in range(cars):
+    speed = int(input())
+    if speed < slowest_car:
+        slowest_car = speed
+    if speed > fastest_car:
+        fastest_car = speed
+    if speed < 30:
+        cars_following_rules += 1
+
+print(fastest_car - slowest_car)
+print(cars_following_rules)

6b76bca84eeb1b48ffeb69ad69fa98e831f1f38e
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 11:10:27 -0400

    Update exercise_35.py

--- exercises/exercise_35.py
+++ exercises/exercise_35.py
@@ -1,1 +1,9 @@
-# Your solution to Exercise 35
+number = int(input())
+
+output = ""
+
+for i in range(int((number - 1) * "9") + 1, int(number * "9") + 1):
+    if i % 2 == 1:
+        output += f"{i} "
+
+print(output)

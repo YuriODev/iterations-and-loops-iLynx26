d4e7f54f3689aa79743284e63e54b637cbc04661
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 12:55:35 -0400

    Update exercise_37.py

--- exercises/exercise_37.py
+++ exercises/exercise_37.py
@@ -1,4 +1,13 @@
 number_1 = int(input())
 number_2 = int(input())
 
-print(f"{number_1 // number_2} {number_1 % number_2}")
+remainder = -1
+divide = -1
+
+for i in range(number_1):
+    if i * number_2 > number_1:
+        remainder = number_1 - number_2 * (i - 1)
+        divide = i - 1
+        break
+
+print(f"{divide} {remainder}")

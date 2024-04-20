a7cf2f47bbcc88a7cb98aba33828d0d66c3aefe6
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 11:27:45 -0400

    Update exercise_35.py

--- exercises/exercise_35.py
+++ exercises/exercise_35.py
@@ -2,7 +2,13 @@ number = int(input())
 
 output = ""
 
-for i in range(int((number - 1) * "9") + 1, int(number * "9") + 1):
+if number != 1:
+    minimum_digit = int((number - 1) * "9") + 1
+else:
+    minimum_digit = 1
+maximum_digit = int(number * "9") + 1
+
+for i in range(maximum_digit, minimum_digit - 1, -1):
     if i % 2 == 1:
         output += f"{i} "
 

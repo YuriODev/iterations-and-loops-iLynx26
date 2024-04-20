bc203d5d02439490284edae2468180d7047fe4a7
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 12:55:45 -0400

    Update exercise_44.py

--- exercises/exercise_44.py
+++ exercises/exercise_44.py
@@ -1,14 +1,14 @@
 biggest_number = 0
-current_number = 0
+current_index = 0
 
 biggest_number_index = -1
 
-infinite_number = 9999
-
-for i in range(infinite_number):
+while True:
     current_number = int(input())
     if current_number >= biggest_number:
         biggest_number = current_number
-        biggest_number_index = i
+        biggest_number_index = current_index
+    current_index += 1
     if current_number == 0:
         print(biggest_number_index)
+        break

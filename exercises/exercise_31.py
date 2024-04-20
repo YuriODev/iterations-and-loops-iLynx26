b74563cdda77475c4c93b52ebc50deb665e646d8
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 11:10:17 -0400

    Update exercise_31.py

--- exercises/exercise_31.py
+++ exercises/exercise_31.py
@@ -1,1 +1,18 @@
-# Your solution to Exercise 31
+lowest_temperature = 999
+
+days = int(input())
+
+temperature = 0
+
+for i in range(days):
+    temperature = int(input())
+    if temperature < lowest_temperature:
+        lowest_temperature = temperature
+
+is_very_cold = lowest_temperature < -15
+
+print(lowest_temperature)
+if is_very_cold:
+    print("Yes")
+else:
+    print("No")

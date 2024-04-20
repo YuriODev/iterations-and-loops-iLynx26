f3c9b8c7dece609a8cec8f7c3a0aa8f45f20339e
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 11:10:55 -0400

    Update exercise_51.py

--- exercises/exercise_51.py
+++ exercises/exercise_51.py
@@ -1,1 +1,12 @@
-# Your solution to Exercise 51
+number_1 = int(input())
+number_2 = int(input())
+
+output = ""
+
+for i in range(number_1, number_2):
+    i1 = i%10
+    i2 = i%100//10
+    i3 = i%1000//100
+    i4 = i//1000
+    if i1 == i2 == i3 or i1 == i2 == i4 or i2 == i3 == i4 or i1 == i3 == i4:
+        output += f"{i} "

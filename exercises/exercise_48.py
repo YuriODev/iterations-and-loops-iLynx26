26da153fad7a24749efdb22cabdfef0da15a4008
Khanieiev Ivan <75529688+iLynx26@users.noreply.github.com>
2024-04-20 11:11:05 -0400

    Update exercise_48.py

--- exercises/exercise_48.py
+++ exercises/exercise_48.py
@@ -1,1 +1,15 @@
-# Your solution to Exercise 48
+n = int(input())
+
+output = 0
+
+for i in range(n):
+    if len(i) == 1:
+        output += 1
+    if len(i) == 2:
+        if n % 10 == n//10:
+            output += 1
+    if len(i) == 3:
+        if n % 10 == n//100:
+            output += 1
+
+print(output)
